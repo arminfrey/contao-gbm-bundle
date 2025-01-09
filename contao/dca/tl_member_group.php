194e79c13a260a674fcefd042b3b3f6cda7700b7
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2025-01-09 15:57:31 +0100

    Update tl_member_group.php

--- contao/dca/tl_member_group.php
+++ contao/dca/tl_member_group.php
@@ -6,5 +6,3 @@ use Arminfrey\ContaoGbmBundle;
  * Delete an according BirthdayMailer configuration, if the member group is deleted.
  */
 $GLOBALS['TL_DCA']['tl_member_group']['config']['ondelete_callback'][] = [SendMail::class, 'deleteConfiguration'];
-
-?>
