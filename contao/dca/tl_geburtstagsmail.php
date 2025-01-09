9985a454b8f4fc4df9bf03d77ccb6443aea92ba5
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2025-01-09 11:00:40 +0100

    Update tl_geburtstagsmail.php

--- contao/dca/tl_geburtstagsmail.php
+++ contao/dca/tl_geburtstagsmail.php
@@ -5,11 +5,11 @@ use Contao\DataContainer;
 use Contao\DC_Table;
 use Contao\Image;
 
-var_dump("dca");
 $GLOBALS['TL_DCA']['tl_geburtstagsmail'] = [
 	// Config
 	'config' => [
-		'dataContainer'           => DC_Table::class,
+		'dataContainer'           => Contao\DC_Table::class,
+		'ptable'                  => 'tl_geburtstagsmail',
 		'enableVersioning'        => true,
 		'sql' => array
 		(
@@ -151,7 +151,6 @@ class tl_geburtstagsmail extends Backend
 {
 	public function addIcon($row, $label)
 	{
-		var_dump("addicon");
 		if (empty($row)) {
     			\Contao\Log::add('Row data is empty for icon generation.', 'ContaoGbmBundle addIcon()', TL_ERROR);
 		}
