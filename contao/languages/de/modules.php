338eefb9f36e5e7ba5ade66c95a5380748f1c2e6
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2025-01-09 10:15:43 +0100

    Create modules.php

---
+++ contao/languages/de/modules.php
@@ -0,0 +1,6 @@
+<?php
+
+/**
+ * Define name and tooltip for preferences (inactive modules)
+ */
+$GLOBALS['TL_LANG']['MOD']['Geburtstagsmail'] = array('Geburtstagsmail', 'Versendet eine Geburtstagsmail an Mitglieder, die am aktuellen Tag Geburtstag haben.');
