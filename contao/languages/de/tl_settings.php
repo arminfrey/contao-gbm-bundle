b60da930e900bc4b6faabef8441afb5b272561db
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2025-01-09 10:17:39 +0100

    Create tl_settings.php

---
+++ contao/languages/de/tl_settings.php
@@ -0,0 +1,8 @@
+<?php
+ 
+$GLOBALS['TL_LANG']['tl_settings']['birthdayMailer_legend']                 = "Geburtstagsmail";
+$GLOBALS['TL_LANG']['tl_settings']['birthdayMailerAllowDuplicates']         = array('Duplikate erlauben', 'Wenn diese Option gewählt ist, werden Geburtstagsmails an ein und dasselbe Mitglieder mit mehreren konfigurierten Mitgliedergruppen mehrfach gesendet (das Mitglieder bekommt dann mehrere Geburtstagsmails).');
+$GLOBALS['TL_LANG']['tl_settings']['birthdayMailerLogDebugInfo']            = array('Zusätzliche Debug Informationen loggen', 'Wenn diese Option gewählt ist, werden zusätzliche Debug Informationen im System-Log eingetragen (pro E-Mail die versendet werden soll ein Eintrag mit allen Inhalten).');
+$GLOBALS['TL_LANG']['tl_settings']['birthdayMailerDeveloperMode']           = array('Entwicklermodus', 'Aktiviert den Entwicklermodus. Emails gehen nur an die Entwickler E-Mail-Adresse.');
+$GLOBALS['TL_LANG']['tl_settings']['birthdayMailerDeveloperModeEmail']      = array('Entwickler E-Mail-Adresse', 'Im Entwicklermodus werden alle E-Mails an diese Adresse umgeleitet.');
+$GLOBALS['TL_LANG']['tl_settings']['birthdayMailerDeveloperModeIgnoreDate'] = array('Geburtsdatum ignorieren', 'Umgeht die Prüfung ob ein Mitglied am aktuellen Tag Geburtstag hat. Es wird für jedes aktive Mitglied eine Email gesendet.');
