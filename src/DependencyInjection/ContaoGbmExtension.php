bdd7ae6eaf39fc5bd442d8b4234a10e0e7e30a0c
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2025-01-09 08:54:31 +0100

    Create ContaoGbmExtension.php

---
+++ src/DependencyInjection/ContaoGbmExtension.php
@@ -0,0 +1,18 @@
+<?php
+// src/DependencyInjection/ContaoGbmExtension.php
+namespace Arminfrey\ContaoGbmBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class ContaoGbmExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        (new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config')))
+            ->load('services.yaml')
+        ;
+    }
+}
