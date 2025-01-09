5f3ab563e4ea2137b07ca4604e15fc4156e1c4d9
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2025-01-09 08:58:33 +0100

    Update ContaoGbmBundle.php

--- src/ContaoGbmBundle.php
+++ src/ContaoGbmBundle.php
@@ -2,8 +2,14 @@
 // src/ContaoGbmBundle.php
 namespace Arminfrey\ContaoGbmBundle;
 
+use App\DependencyInjection\ContaoGbmExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class ContaoGbmBundle extends AbstractBundle
 {
+  public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new ContaoGbmExtension();
+    }
 }
