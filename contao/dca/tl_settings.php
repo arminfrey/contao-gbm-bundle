60136780fda1dd33f6df29a8efdabfe6fd9645f1
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2025-01-09 10:02:06 +0100

    Create tl_settings.php

---
+++ contao/dca/tl_settings.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Add to palette
+ */
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['__selector__'][] = 'birthdayMailerDeveloperMode';
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] .= ';{birthdayMailer_legend},birthdayMailerAllowDuplicates, birthdayMailerLogDebugInfo, birthdayMailerDeveloperMode;';
+$GLOBALS['TL_DCA']['tl_settings']['subpalettes']['birthdayMailerDeveloperMode'] = 'birthdayMailerDeveloperModeEmail, birthdayMailerDeveloperModeIgnoreDate';
+
+/**
+ * Add fields
+ */
+$GLOBALS['TL_DCA']['tl_settings']['fields']['birthdayMailerAllowDuplicates'] = [
+	'label'     => &$GLOBALS['TL_LANG']['tl_settings']['birthdayMailerAllowDuplicates'],
+	'inputType' => 'checkbox',
+	'eval'      => ['tl_class'=>'w50']
+];
+
+$GLOBALS['TL_DCA']['tl_settings']['fields']['birthdayMailerLogDebugInfo'] = [
+	'label'     => &$GLOBALS['TL_LANG']['tl_settings']['birthdayMailerLogDebugInfo'],
+	'inputType' => 'checkbox',
+	'eval'      => ['tl_class'=>'w50']
+];
+
+$GLOBALS['TL_DCA']['tl_settings']['fields']['birthdayMailerDeveloperMode'] = [
+	'label'                   => &$GLOBALS['TL_LANG']['tl_settings']['birthdayMailerDeveloperMode'],
+	'inputType'               => 'checkbox',
+	'eval'                    => ['submitOnChange'=>true, 'tl_class'=>'w50 clr']
+];
+
+$GLOBALS['TL_DCA']['tl_settings']['fields']['birthdayMailerDeveloperModeEmail'] = [
+	'label'                   => &$GLOBALS['TL_LANG']['tl_settings']['birthdayMailerDeveloperModeEmail'],
+	'inputType'               => 'text',
+	'eval'                    => ['mandatory'=>true, 'rgxp'=>'email', 'tl_class'=>'w50 clr']
+];
+
+$GLOBALS['TL_DCA']['tl_settings']['fields']['birthdayMailerDeveloperModeIgnoreDate'] = [
+	'label'                   => &$GLOBALS['TL_LANG']['tl_settings']['birthdayMailerDeveloperModeIgnoreDate'],
+	'inputType'               => 'checkbox',
+	'eval'                    => ['tl_class'=>'w50']
+];
