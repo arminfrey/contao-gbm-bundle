73002de66d70f864116a357e77562826748bf6ad
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2025-01-09 09:38:58 +0100

    Create ContaoGbmModel.php

---
+++ src/Model/ContaoGbmModel.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arminfrey\ContaoGbmBundle\Model;
+
+use Contao\Model;
+
+class ContaoGbmModel extends Model
+{
+    protected static $strTable = 'tl_geburtstagsmail';
+
+}
