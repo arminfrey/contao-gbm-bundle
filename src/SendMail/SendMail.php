506c87b0ccf1077c0e55f62d255fc0042eafefeb
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2025-01-16 15:33:25 +0100

    Update SendMail.php

--- src/SendMail/SendMail.php
+++ src/SendMail/SendMail.php
@@ -38,9 +38,9 @@ class SendMail
 			
 			// Create template object
 			$objTemplate = new BackendTemplate('be_geburtstagsmail');
-			//$cleanedUrl = str_replace('&key=sendBirthdayMail', '', $this->request);
-			//var_dump(System::getContainer()->get('request_stack')->getCurrentRequest());
-			$cleanedUrl = str_replace('&', '&amp;', $cleanedUrl);
+			$cleanedUrl = str_replace($this->request->query->get('key'), '', $this->request->getUri());
+			$cleanedUrl = str_replace($this->request->query->get('rt'), '', $cleanedUrl);
+			$cleanedUrl = str_replace($this->request->query->get('ref'), '', $cleanedUrl);
 			$escapedTitle = StringUtil::specialchars($GLOBALS['TL_LANG']['MSC']['backBT']);
 			$objTemplate->backLink = '<a href="' . $cleanedUrl . '" class="header_back" title="' . $escapedTitle . '" accesskey="b">' . $escapedTitle . '</a>';
 			//$objTemplate->backLink = '<a href="'.\ampersand(str_replace('&key=sendBirthdayMail', '', $this->Environment->request)).'" class="header_back" title="'.specialchars($GLOBALS['TL_LANG']['MSC']['backBT']).'" accesskey="b">'.$GLOBALS['TL_LANG']['MSC']['backBT'].'</a>';
