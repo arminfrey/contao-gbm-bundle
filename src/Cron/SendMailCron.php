50fd3eb8fea24e1b7a4d8bea970c829983f0d0bc
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2025-01-27 11:02:38 +0100

    Update SendMailCron.php

--- src/Cron/SendMailCron.php
+++ src/Cron/SendMailCron.php
@@ -2,7 +2,7 @@
 
 namespace Arminfrey\ContaoGbmBundle\Cron;
 
-use Arminfrey\ContaoGbmBundle\SendMail;
+use Arminfrey\ContaoGbmBundle\SendMail\SendMail;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsCronJob;
 use Contao\CoreBundle\Framework\ContaoFramework;
 
@@ -10,7 +10,7 @@ use Contao\CoreBundle\Framework\ContaoFramework;
 class SendMailCron
 {
     public function __construct(
-        private readonly SendMail $sendMail,
+        private readonly SendMail $sendMail
     ) {
     }
 
