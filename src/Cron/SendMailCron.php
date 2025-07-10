00812a697a914fed1a7bff4249515e8a1c285f52
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2025-07-10 08:44:03 +0200

    Update SendMailCron.php

--- src/Cron/SendMailCron.php
+++ src/Cron/SendMailCron.php
@@ -5,8 +5,10 @@ namespace Arminfrey\ContaoGbmBundle\Cron;
 use Arminfrey\ContaoGbmBundle\SendMail\SendMail;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsCronJob;
 use Contao\CoreBundle\Framework\ContaoFramework;
+use Symfony\Component\Lock\LockFactory;
+use Symfony\Component\Lock\Store\FlockStore;
 
-#[AsCronJob('0 4-8 * * *')]
+#[AsCronJob('8 6 * * *')]
 class SendMailCron
 {
     public function __construct(
@@ -16,6 +18,20 @@ class SendMailCron
 
     public function __invoke(): void
     {
-        $this->sendMail->sendBirthdayMail();
+        $store = new FlockStore(); // Oder z.B. PDOStore
+        $factory = new LockFactory($store);
+        $lock = $factory->createLock('send_birthday_mail_lock', 300); // 5 Minuten
+        
+        if (!$lock->acquire()) {
+            // Bereits ein anderer Prozess läuft
+            return;
+        }
+        
+        try {
+            $this->sendMail->sendBirthdayMail();
+        } 
+        finally {
+            $lock->release();
+        }
     }
 }
